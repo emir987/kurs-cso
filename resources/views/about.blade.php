1afe7807fbd7004f43c5eeb43a7e6d00d34ec9c6
emir987 <emir.kosuta@gmail.com>
2021-12-17 13:11:22 +0100

    init laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('title', 'About')
+
+@section('body')
+    <h1>About page</h1>
+@endsection
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('title', 'Homepage')
+
+@section('body')
+    <h1>Homepage</h1>
+@endsection
