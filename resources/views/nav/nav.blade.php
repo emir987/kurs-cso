978d1d39319352e4c0ee0fdf9aa3c5b70c2a0e11
emir987 <emir.kosuta@gmail.com>
2021-12-17 16:22:55 +0100

    add routes to navigation

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -1,14 +1,8 @@
 <ul class="nav justify-content-center">
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
+        <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Homepage</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+        <a class="nav-link" href="{{ route('about') }}">About</a>
     </li>
 </ul>
